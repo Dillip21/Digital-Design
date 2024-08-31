f411fd73d14b176cc229bc310719ddeb46d462dc
Dillip21 <sidd15021@gmail.com>
2024-08-31 23:44:56 +0530

    Add files via upload
    ASIC-Based Image Processing Module for Real-Time Image Enhancement

---
+++ Image_Processing/parameter.v
@@ -0,0 +1,8 @@
+`define INPUTFILENAME		 "SampleImg.hex" // Input file name
+`define OUTPUTFILENAME		 "SampleImgTHLD.bmp"		// Output file name
+
+// Choose the operation of code 
+//`define BRIGHTNESS_OPERATION
+//`define INVERT_OPERATION
+//`define BLACKandWHITE_OPERATION
+`define THRESHOLD_OPERATION 
